d01c0bf7af0e451f2f3d7bc2091164b7e21392b9
zenin5479 <zenin5479@gmail.com>
2023-10-21 16:48:37 +0300

    Update 0.2 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -21,30 +21,34 @@ namespace SortingListObjects
             lst.Add(new Player("Ben", 100));
             lst.Add(new Player("Deniel", 88));
 
-
+            // Сортировка с помощью list<t>. Sort() - метод
+            // Сортировка по одному элементу "" в порядке возрастания: Total
             lst.Sort(delegate (Player x, Player y)
             {
                 return x.Total.CompareTo(y.Total);
             });
 
+            // Сортировка по одному элементу "" в порядке убывания: Total
             lst.Sort(delegate (Player x, Player y)
             {
                 return y.Total.CompareTo(x.Total);
             });
 
+            // Сортировка по нескольким элементам:
             lst.Sort(delegate (Player x, Player y)
             {
-                // Sort by total in descending order
+                // Сортировка по общему количеству в порядке убывания
                 int a = y.Total.CompareTo(x.Total);
 
-                // Both player has the same total.
-                // Sort by name in ascending order
+                // У обоих игроков одинаковый Total.
+                // Сортировка по названию в порядке возрастания
                 if (a == 0)
                     a = x.Name.CompareTo(y.Name);
 
                 return a;
             });
 
+            // Сортировка с помощью Linq требует меньшего количества синтаксиса.
             var result1 = lst.OrderBy(a => a.Total);
 
             var result3 = lst.OrderBy(a => a.Total).ThenBy(a => a.Name);
